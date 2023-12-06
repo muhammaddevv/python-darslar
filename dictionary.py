2395193639c4cae477c5136a679a61dcf28f3bc2
SHeraxanov Muham(shoxrux) <152672271+muhammaddevv@users.noreply.github.com>
2023-12-06 07:56:17 +0500

    yangi dars qo'shildi
    dictionary darsini qo'shdim

---
+++ dictionary.py
@@ -0,0 +1,89 @@
+#avto = {'model':'nexia', 'rang':'oq'}
+# print(avto)
+# print(avto['model'])
+# print(avto['rang'])
+
+
+#til = {'apple':'olma', 'lemon':'limon', 'apricot':'o\'rik'}
+# print(til['snake'])
+
+#mevalar = {'olma':15000, 'banan':22000, 'qovun':35000}
+#print(f"olmaning narxi {mevalar['olma']} so'm")
+#print(f"qovunning narxi {mevalar['qovun']} so'm")
+
+
+#talaba = {'ismi':'abdulloh abdulmannon', 'yoshi':25, 't_yili':1998}
+# print(f"{talaba['ismi'].title()},\
+#   {talaba['t_yili']} yilda tug'ilgan,\
+#   {talaba['yoshi']} yoshda")
+  ## yangi so'z va qiymat qo'shish
+# talaba['kurs'] = 3
+# talaba['fakultet'] = 'falsafa'
+#print(talaba)
+  ## elementni o'chirish
+# del talaba['yoshi']
+# print(talaba)
+
+# qiymatni o'zgartirish
+# talaba['ismi'] = 'muhammad'
+# print(talaba)
+
+
+# bo'sh lug'at
+
+# student = {}
+# student['ism'] = 'xolid'
+# student['yosh'] = 25
+# student['fakultet'] = 'siyosat'
+# student['kurs'] = 2
+# #print(student)
+
+# student['kurs'] = 4
+# print(student)
+
+
+# telefonlar = {
+#     'ali':'iphone x',
+#     'vali':'galaxy s9',
+#     'olim':'mi 10 pro',
+#     'orif':'nokia 3310'
+#     }
+
+# get metodi
+
+# phone = telefonlar.get('olim','bunday ism mavjud emas')
+# print(phone)
+
+# fruit = til.get('banana','bunday meva mavjud emas')
+# print(fruit)
+
+# kalit so'zdan keyin qiymat berilmasa none chiqadi
+# phone = telefonlar.get('baxti')
+# print(phone)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
